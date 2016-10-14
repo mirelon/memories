78de4b36a9dcbab50a272c57579f3d15f74a83f0
mirelon <miso@github.ksp.sk>
2016-10-14 13:14:10 +0200

    Fixed adding people to event

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -12,7 +12,7 @@ ActiveAdmin.register Event do
         event.people.map { |p| p.name }.join(", ").html_safe
       end
       row :resources do
-        event.resources.map { |r| link_to r.name, r.url, target: '_blank' }.join('').html_safe
+        event.resources.map { |r| link_to r.name, r.url, target: '_blank' }.join('<br/>').html_safe
       end
     end
   end
@@ -26,7 +26,7 @@ ActiveAdmin.register Event do
       f.input :date_to, as: :datepicker
       f.input :text
       f.has_many :event_people, allow_destroy: true do |deg|
-        deg.input :person
+        deg.input :person, collection: Person.order(:firstname, :lastname).map{|p| [p.name, p.id]}
       end
       f.has_many :resources, allow_destroy: true do |deg|
         deg.input :url
